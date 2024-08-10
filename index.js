1b3f802e1cf09709cf0fa1b62a7d844cda935159
shawon13 <rakibarg1519@gmail.com>
2024-08-10 15:19:51 +0600

    change data base uri

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = 'mongodb://localhost:27017'
-// const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.madtkr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = 'mongodb://localhost:27017'
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.madtkr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
